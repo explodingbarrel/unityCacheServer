7bf5887e74b5baf051f67f3babdbdafcd050cf86
unity <unity@as5.corp.kabam.com>
2016-12-17 19:05:42 -0800

    fixed run script path

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-./current/RunLinux.sh --path /data/cache5.0 --legacypath /data/legacy --size 500000000 > /var/log/unityCacheServer.log 2>&1
+./RunLinux.sh --path /data/cache5.0 --legacypath /data/legacy --size 500000000 > /var/log/unityCacheServer.log 2>&1
