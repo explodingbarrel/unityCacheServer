dd41f98f83c5903fec8db94e30d561abffdfebed
mo ko <email>
2017-02-02 19:53:17 -0800

    changed cache size to 100G

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 killall node
-./RunLinux.sh --path /data/cache5.0 --legacypath /data/legacy --size 500000000 --nolegacy --iplist '10\.(210\.0\.[6|7][0-9]|211\.60\.16)' > /var/log/unityCacheServer.log 2>&1
+./RunLinux.sh --path /data/cache5.0 --legacypath /data/legacy --size 100000000000 --nolegacy --iplist '10\.(210\.0\.[6|7][0-9]|211\.60\.16)' > /var/log/unityCacheServer.log 2>&1
