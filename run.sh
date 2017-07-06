f37f2f2f4340388e93d229e1c3b8d170b4414b8d
mo ko <email>
2017-07-05 18:39:37 -0700

    updated van-build00 ip address

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 killall node
-./RunLinux.sh --path /data/cache5.0 --legacypath /data/legacy --size 100000000000 --nolegacy --iplist '10\.(210\.0\.[6|7][0-9]|211\.60\.(16|27))' > /var/log/unityCacheServer.log 2>&1
+./RunLinux.sh --path /data/cache5.0 --legacypath /data/legacy --size 100000000000 --nolegacy --iplist '10\.(210\.0\.[6|7][0-9]|211\.60\.(18|27))' > /var/log/unityCacheServer.log 2>&1
 #./RunLinux.sh --path /data/cache5.0 --legacypath /data/legacy --size 100000000000 --nolegacy > /var/log/unityCacheServer.log 2>&1
