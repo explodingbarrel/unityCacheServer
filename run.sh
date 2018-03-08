8b646fbeeca196215d19633cedfaa1a95588aaa0
mo ko <email>
2018-03-07 19:34:20 -0800

    added new build00 and build-pc ip

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 killall node
-./RunLinux.sh --path /data/cache5.0 --legacypath /data/legacy --size 100000000000 --nolegacy --iplist '10\.(210\.0\.[6|7][0-9]|211\.0\.(69|73))' > /var/log/unityCacheServer.log 2>&1
+./RunLinux.sh --path /data/cache5.0 --legacypath /data/legacy --size 100000000000 --nolegacy --iplist '10\.(210\.0\.[6|7][0-9]|211\.0\.(176|119))' > /var/log/unityCacheServer.log 2>&1
 #./RunLinux.sh --path /data/cache5.0 --legacypath /data/legacy --size 100000000000 --nolegacy > /var/log/unityCacheServer.log 2>&1
