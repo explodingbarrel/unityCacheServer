1ef5661a6f631f0b88494285933bf79c1357749d
William Chow <wchow@kabaminc.com>
2018-07-13 09:10:44 -0700

    debug code clean up.

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 killall node
-unity-cache-server -P /data/cache5.0/ -l 5 > /var/log/unity/unityCacheServer.log 2>&1 &
+unity-cache-server -P /data/cache5.0/ > /var/log/unity/unityCacheServer.log 2>&1 &
