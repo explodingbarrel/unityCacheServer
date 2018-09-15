889e1743ed4652e118aa2c8d9d258d43b74f9d93
mo ko <email>
2018-09-14 23:20:44 -0700

    added a start up script

---
+++ scripts/unityCacheServer.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+export NODE_PATH=/usr/lib/node_modules
+basedir=$(dirname "$(echo "$0" | sed -e 's,\\,/,g')")
+
+case `uname` in
+    *CYGWIN*) basedir=`cygpath -w "$basedir"`;;
+esac
+
+if [ -x "$basedir/node" ]; then
+  "$basedir/node"  "/opt/unityCacheServer/main.js" "$@" > /var/log/unityCacheServer.log 2>&1
+  ret=$?
+else
+  node  "/opt/unityCacheServer/main.js" "$@" > /var/log/unityCacheServer.log 2>&1
+  ret=$?
+fi
+exit $ret
